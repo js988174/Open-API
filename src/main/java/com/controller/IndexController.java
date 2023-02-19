5e506e4847b8799b13edbffa0c61878155097f51
석성희 <sunghee119z@naver.com>
2023-02-19 17:04:50 +0900

    d
    d

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("")
+    public String index(){
+        System.out.println("인덱스 들어옴");
+        return "/index";
+    }
+}
