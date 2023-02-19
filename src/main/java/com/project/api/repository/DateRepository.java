e6940f22d27cdf6aa5f23dc1e33eb7d42e1a8231
석성희 <sunghee119z@naver.com>
2023-02-19 17:52:34 +0900

    레퍼지토리.서비스
    레퍼지토리.서비스

---
+++ src/main/java/com/project/api/repository/DateRepository.java
@@ -0,0 +1,9 @@
+package com.project.api.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class DateRepository {
+}
