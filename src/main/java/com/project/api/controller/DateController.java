36e76887573247fca0ee13e5006b9e294fba907a
석성희 <sunghee119z@naver.com>
2023-02-21 20:36:34 +0900

    d
    d

--- src/main/java/com/project/api/controller/DateController.java
+++ src/main/java/com/project/api/controller/DateController.java
@@ -1,8 +1,16 @@
 package com.project.api.controller;
 
+import com.project.api.vo.MemberVo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DateController {
 
+    @GetMapping("/test")
+    public MemberVo memberVo(){
+        MemberVo memberVo = new MemberVo(1,"김경선");
+        return memberVo;
+    }
 }
--- src/main/java/com/project/api/vo/MemberVo.java
+++ src/main/java/com/project/api/vo/MemberVo.java
@@ -1,4 +1,14 @@
 package com.project.api.vo;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class MemberVo {
+
+    private Integer memberNo;
+    private String memberName;
+
 }
