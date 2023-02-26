3bdd2dd884d87192f649b3e5f009c2757db291e8
석성희 <sunghee119z@naver.com>
2023-02-26 18:30:36 +0900

    d
    d

--- src/main/java/com/project/api/apiclass/WeatherAPI.java
+++ src/main/java/com/project/api/apiclass/WeatherAPI.java
@@ -18,16 +18,14 @@ public class WeatherAPI {
 
     public Object test() throws Exception{
         String enc = "UTF-8";
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey",enc) +
-                "=aXRvEuydsB3VHnBbhOSxVlV082l%2BZFFBg1x5mnyiIYNJAPMAga3eT8Ij74RZxl%2F7s0VLFZgna67sdG5c7FMytA%3D%3D"); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo",enc) + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows",enc) + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("dataType",enc) + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
-        urlBuilder.append("&" + URLEncoder.encode("base_date",enc) + "=" + URLEncoder.encode("20230225", "UTF-8")); /*‘21년 6월 28일발표*/
-        urlBuilder.append("&" + URLEncoder.encode("base_time",enc) + "=" + URLEncoder.encode("0500", "UTF-8")); /*05시 발표*/
-        urlBuilder.append("&" + URLEncoder.encode("nx",enc) + "=" + URLEncoder.encode("55", "UTF-8")); /*예보지점의 X 좌표값*/
-        urlBuilder.append("&" + URLEncoder.encode("ny",enc) + "=" + URLEncoder.encode("127", "UTF-8")); /*예보지점의 Y 좌표값*/
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/WthrChartInfoService/getSurfaceChart"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" +
+                "aXRvEuydsB3VHnBbhOSxVlV082l%2BZFFBg1x5mnyiIYNJAPMAga3eT8Ij74RZxl%2F7s0VLFZgna67sdG5c7FMytA%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON)*/
+        urlBuilder.append("&" + URLEncoder.encode("code","UTF-8") + "=" + URLEncoder.encode("24", "UTF-8")); /*지상코드*/
+        urlBuilder.append("&" + URLEncoder.encode("time","UTF-8") + "=" + URLEncoder.encode("20230226", "UTF-8")); /*년월일(YYYYMMDD)*/
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -50,6 +48,8 @@ public class WeatherAPI {
 
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject =(JSONObject) jsonParser.parse(sb.toString());
+
+
         return jsonObject;
     }
 
