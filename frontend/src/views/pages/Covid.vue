d5b8931aaccd731ac996ca73c0c5985d32a756c6
js988174 <76925402+js988174@users.noreply.github.com>
2023-02-28 17:46:34 +0900

    1차 접종현황 차트 추가

--- frontend/src/views/pages/Covid.vue
+++ frontend/src/views/pages/Covid.vue
@@ -1,55 +1,59 @@
 <template>
-  <div>
-    <CAlert color="primary">{{ covidUpdateTime }}</CAlert>
-    <CRow>
-      <CCol :xs="4">
-        <CWidgetStatsC
-          class="mb-3"
-          :value="covidKorea.totalCnt"
-          inverse
-          color="info"
-          :progress="{ value: 100 }"
-          title="국내 확진자"
-        >
-          <template #icon><CIcon icon="cil-people" height="36" /></template>
-        </CWidgetStatsC>
-      </CCol>
-      <CCol :xs="4">
-        <CWidgetStatsC
-          class="mb-3"
-          :value="covidKorea.incDec"
-          inverse
-          color="success"
-          :progress="{ value: 75 }"
-          title="당일 확진자"
-        >
-          <template #icon
-            ><CIcon icon="cil-user-follow" height="36"
-          /></template>
-        </CWidgetStatsC>
-      </CCol>
-      <CCol :xs="4">
-        <CWidgetStatsC
-          class="mb-3"
-          :value="covidKorea.incDecF"
-          inverse
-          color="info"
-          :progress="{ value: 5 }"
-          title="전일 대비 증가"
-        >
-          <template #icon
-            ><CIcon icon="cil-user-follow" height="36"
-          /></template>
-        </CWidgetStatsC>
-      </CCol>
-    </CRow>
+  <CAlert color="primary">{{ covidUpdateTime }}</CAlert>
+  <CRow>
+    <CCol :xs="4">
+      <CWidgetStatsC
+        class="mb-3"
+        :value="covidKorea.totalCnt"
+        inverse
+        color="info"
+        :progress="{ value: 100 }"
+        title="국내 확진자"
+      >
+        <template #icon><CIcon icon="cil-people" height="36" /></template>
+      </CWidgetStatsC>
+    </CCol>
+    <CCol :xs="4">
+      <CWidgetStatsC
+        class="mb-3"
+        :value="covidKorea.incDec"
+        inverse
+        color="success"
+        :progress="{ value: 75 }"
+        title="당일 확진자"
+      >
+        <template #icon><CIcon icon="cil-user-follow" height="36" /></template>
+      </CWidgetStatsC>
+    </CCol>
+    <CCol :xs="4">
+      <CWidgetStatsC
+        class="mb-3"
+        :value="covidKorea.incDecF"
+        inverse
+        color="info"
+        :progress="{ value: 5 }"
+        title="전일 대비 증가"
+      >
+        <template #icon><CIcon icon="cil-user-follow" height="36" /></template>
+      </CWidgetStatsC>
+    </CCol>
+  </CRow>
+  <CRow>
     <CCol :md="6" class="mb-4">
       <CCard>
-        <CCardHeader>Doughnut Chart</CCardHeader>
+        <CCardHeader>지역별 확진자 수</CCardHeader>
         <CCardBody> <CChartDoughnut :data="defaultData" /></CCardBody>
       </CCard>
     </CCol>
-  </div>
+    <CCol :md="6" class="mb-4">
+      <CCard>
+        <CCardHeader> 예방 1차 접종 현황 </CCardHeader>
+        <CCardBody>
+          <CChartDoughnut :data="defaultVaccineData" />
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
 </template>
 
 <script>
@@ -85,13 +89,42 @@ export default {
         ],
       }
     },
+    defaultVaccineData() {
+      return {
+        labels: ['부산', '대구', '강원', '경기', '인천', '서울', '울산'],
+        datasets: [
+          {
+            backgroundColor: [
+              '#41B883',
+              '#E46651',
+              '#00D8FF',
+              '#DD1B16',
+              '#0076A3',
+              '#FFA500',
+              '#8B00FF',
+            ],
+            data: [
+              this.busanVaccine,
+              this.daeguVaccine,
+              this.gangwonVaccine,
+              this.gyeonggiVaccine,
+              this.incheonVaccine,
+              this.seoulVaccine,
+              this.ulsanVaccine,
+            ],
+          },
+        ],
+      }
+    },
   },
   mounted() {
     this.covidList()
+    this.covidVaccineList()
   },
   data() {
     return {
       covidDataList: [],
+      covidVaccineDataList: [],
       covidKorea: [],
       covidUpdateTime: String,
       busan: '',
@@ -101,6 +134,13 @@ export default {
       incheon: '',
       seoul: '',
       ulsan: '',
+      busanVaccine: '',
+      daeguVaccine: '',
+      gangwonVaccine: '',
+      gyeonggiVaccine: '',
+      incheonVaccine: '',
+      seoulVaccine: '',
+      ulsanVaccine: '',
     }
   },
   methods: {
@@ -118,9 +158,19 @@ export default {
         this.incheon = response.data.incheon.incDec
         this.seoul = response.data.seoul.incDec
         this.ulsan = response.data.ulsan.incDec
+      })
+    },
+    covidVaccineList() {
+      this.$axios.get('/api/covid/vaccine').then((response) => {
+        this.covidVaccineDataList = response.data
 
-        console.log('updateTIme', this.covidKorea)
-        console.log('covid19', this.covidDataList)
+        this.busanVaccine = response.data.busan.vaccine_1.vaccine_1
+        this.daeguVaccine = response.data.daegu.vaccine_1.vaccine_1
+        this.gangwonVaccine = response.data.gangwon.vaccine_1.vaccine_1
+        this.gyeonggiVaccine = response.data.gyeonggi.vaccine_1.vaccine_1
+        this.incheonVaccine = response.data.incheon.vaccine_1.vaccine_1
+        this.seoulVaccine = response.data.seoul.vaccine_1.vaccine_1
+        this.ulsanVaccine = response.data.ulsan.vaccine_1.vaccine_1
       })
     },
   },
--- src/main/java/com/project/api/controller/CovidController.java
+++ src/main/java/com/project/api/controller/CovidController.java
@@ -47,7 +47,41 @@ public class CovidController {
         }
         rd.close();
         conn.disconnect();
-        result = sb.toString();
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject =(JSONObject) jsonParser.parse(sb.toString());
+
+        return jsonObject;
+    }
+
+    @GetMapping("/covid/vaccine")
+    public Object covidVaccine() throws Exception {
+
+        String apiURL = "https://api.corona-19.kr/korea/vaccine/";
+        String authKey = "berRulMsJy2PTaBXSYv1j9hExcnIdQqDp";
+        String result = "";
+
+        String urlBuilder = apiURL + "?" + URLEncoder.encode("serviceKey", StandardCharsets.UTF_8) + "=" + authKey;
+
+        URL url = new URL(urlBuilder);
+        System.out.println(url);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
 
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject =(JSONObject) jsonParser.parse(sb.toString());
