2d6beef6d9d36e0793664ddeec41810efb616656
js988174 <76925402+js988174@users.noreply.github.com>
2023-03-03 16:19:45 +0900

    tag 제거 , margin 추가

--- frontend/src/views/pages/Movie.vue
+++ frontend/src/views/pages/Movie.vue
@@ -9,7 +9,12 @@
     <br />
     <CButton @click="clickSearch()" color="success">검색</CButton>
     <CRow>
-      <CCard style="width: 18rem" v-for="item in movieDataList" :key="item">
+      <CCard
+        style="width: 18rem"
+        :style="{ margin: 15 + 'px' }"
+        v-for="item in movieDataList"
+        :key="item"
+      >
         <CCardImage
           orientation="top"
           :src="item.image"
