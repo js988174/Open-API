5fae9d5e87a1e3068aeb51433b456eeda69b710e
js988174 <76925402+js988174@users.noreply.github.com>
2023-03-12 20:38:24 +0900

    회원 생성 test

--- src/test/java/com/project/api/controller/MemberControllerTest.java
+++ src/test/java/com/project/api/controller/MemberControllerTest.java
@@ -1,7 +1,67 @@
 package com.project.api.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.api.crypto.PasswordEncoderCustom;
+import com.project.api.repository.MemberRepository;
+import com.project.api.vo.MemberVo;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class MemberControllerTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    PasswordEncoderCustom passwordEncoder;
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private MockMvc mvc;
+
+    @BeforeEach
+    void clean() {
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("회원 생성")
+    void Hello_Test() throws Exception {
+        //given
+        String password = "1234";
+        String encodedPassword = passwordEncoder.encode(password);
+
+        MemberVo memberVo = MemberVo.builder()
+                .id("테스트 계정2")
+                .password(encodedPassword)
+                .name("테스트 이름1")
+                .build();
+
+
+        String url = "http://localhost:" + port + "/api/member/create";
+
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(memberVo)))
+                .andExpect(status().isOk());
+    }
+
+
 
 }
