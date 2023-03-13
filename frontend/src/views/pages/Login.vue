1a31a11946be410d4727dbf24cc27622e9fea8da
js988174 <76925402+js988174@users.noreply.github.com>
2023-03-13 20:30:12 +0900

    로그인 정보 받아오기

--- frontend/src/views/pages/Login.vue
+++ frontend/src/views/pages/Login.vue
@@ -85,12 +85,9 @@ export default {
           if (response.data.errorCode === 400) {
             alert(response.data.errorMessage)
           } else {
-            this.$axios.create({
-              baseURL: process.env.VUE_APP_API_URL,
-              headers: {
-                Authrozation: response.data.token,
-              },
-            })
+            this.$axios.defaults.headers.common[
+              'X-AUTH-TOKEN'
+            ] = ` ${response.data.token}`
             alert('로그인이 완료되었습니다. 메인 화면으로 돌아갑니다')
             this.myInfo()
             this.$router.push('/dashboard')
