1e0b558f34eb1fec62da8daf1851fd65e23659cc
js988174 <76925402+js988174@users.noreply.github.com>
2023-04-05 21:55:17 +0900

    Socket ChatHandler

--- src/main/java/com/project/api/config/socket/WebSocketConfig.java
+++ src/main/java/com/project/api/config/socket/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.project.api.config;
+package com.project.api.config.socket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -10,6 +10,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -20,4 +22,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").withSockJS();
     }
+
+
 }
