87f3edf32fe3abb7d641a059bfb290033a1f9686
js988174 <76925402+js988174@users.noreply.github.com>
2023-04-06 17:42:50 +0900

    Socket Chat

--- frontend/src/views/pages/Webrtc.vue
+++ frontend/src/views/pages/Webrtc.vue
@@ -1,20 +1,16 @@
 <template>
-  <div>
-    <h1>채팅</h1>
-    <div>
-      <div v-for="message in messages" :key="message.id">
-        <strong>{{ message.username }}:</strong> {{ message.text }}
-      </div>
+  <div id="app">
+    내용: <input v-model="message" type="text" @keyup="sendMessage" />
+    <div v-for="(item, idx) in recvList" :key="idx">
+      <h3>내용: {{ item.content }}</h3>
     </div>
-    <input
-      v-model="message"
-      @keyup.enter="send"
-      placeholder="메시지를 입력하세요"
-    />
   </div>
 </template>
 
 <script>
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
+
 export default {
   name: 'Webrtc',
   data() {
@@ -22,25 +18,59 @@ export default {
       messages: [],
       message: '',
       username: '',
-      ws: null,
+      recvList: [],
     }
+  },
+  created() {
+    this.connect()
   },
   mounted() {
-    prompt('닉네임을 입력하ss세요:')
-    this.ws = new WebSocket('ws://localhost:8086/chat')
-    this.ws.onmessage = (event) => {
-      this.messages.push(JSON.parse(event.data))
-    }
+    // prompt('닉네임을 입력하ss세요:')
+    // this.ws = new WebSocket('ws://localhost:8086/chat')
+    // this.ws.onmessage = (event) => {
+    //   this.messages.push(JSON.parse(event.data))
+    // }
   },
   methods: {
-    send() {
-      if (this.message !== '') {
-        const message = {
-          content: this.message,
-        }
-        this.ws.send(JSON.stringify(message))
+    sendMessage(e) {
+      if (e.keyCode === 13 && this.userName !== '' && this.message !== '') {
+        this.send()
         this.message = ''
       }
+    },
+    send() {
+      console.log('Send message:' + this.message)
+      if (this.stompClient && this.stompClient.connected) {
+        const msg = {
+          content: this.message,
+        }
+        this.stompClient.send('/receive', JSON.stringify(msg), {})
+      }
+    },
+    connect() {
+      const serverURL = 'http://localhost:8086'
+      let socket = new SockJS(serverURL)
+      this.stompClient = Stomp.over(socket)
+      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+      this.stompClient.connect(
+        {},
+        (frame) => {
+          // 소켓 연결 성공
+          this.connected = true
+          console.log('소켓 연결 성공', frame)
+          // 서버의 메시지 전송 endpoint를 구독합니다.
+          // 이런형태를 pub sub 구조라고 합니다.
+          this.stompClient.subscribe('/topic', (res) => {
+            // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+            this.recvList.push(JSON.parse(res.body))
+          })
+        },
+        (error) => {
+          // 소켓 연결 실패
+          console.log('소켓 연결 실패', error)
+          this.connected = false
+        },
+      )
     },
   },
 }
--- src/main/java/com/project/api/config/socket/WebSocketConfig.java
+++ src/main/java/com/project/api/config/socket/WebSocketConfig.java
@@ -19,6 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 }
